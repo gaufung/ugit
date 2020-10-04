156e87b9cf6dbc8e79baeb892a15f59ba99aac83
gaufung <gaufung@outlook.com>
2020-10-04 21:12:02 +0800

    first commit

---
+++ ugit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ugit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
