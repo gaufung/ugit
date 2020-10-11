21b85b25620e9b00bc7b7ec334a1d3190edc9392
gaufung <gaufung@outlook.com>
2020-10-11 10:34:16 +0800

    #11 and #12 hash all files and write tree object

--- test/UgitTest/BaseOperatorTests.cs
+++ test/UgitTest/BaseOperatorTests.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.Collections.Generic;
 using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
 
 namespace Ugit
 {
@@ -14,6 +15,8 @@ namespace Ugit
 
         private Mock<IDirectory> directoryMock;
 
+        private Mock<IFile> fileMock;
+
         private IBaseOperator baseOperator;
 
         [TestInitialize]
@@ -22,6 +25,7 @@ namespace Ugit
             fileSystemMock = new Mock<IFileSystem>();
             dataProviderMock = new Mock<IDataProvider>();
             directoryMock = new Mock<IDirectory>();
+            fileMock = new Mock<IFile>();
             baseOperator = new BaseOperator(fileSystemMock.Object, dataProviderMock.Object);
         }
 
@@ -34,10 +38,50 @@ namespace Ugit
             {
                 @".\sub\ugit.txt"
             });
+            byte[] helloData = Encoding.UTF8.GetBytes("Hello World");
+            byte[] ugitData = Encoding.UTF8.GetBytes("Hello Ugit");
+            fileMock.Setup(f => f.ReadAllBytes(@".\hello.txt")).Returns(helloData);
+            fileMock.Setup(f => f.ReadAllBytes(@".\sub\ugit.txt")).Returns(ugitData);
             fileSystemMock.Setup(f => f.Directory).Returns(directoryMock.Object);
-            baseOperator.WriteTree();
+            fileSystemMock.Setup(f => f.File).Returns(fileMock.Object);
+            dataProviderMock.Setup(f => f.GitDir).Returns(".ugit");
+            string subStree = "blob bar ugit.txt";
+
+            string tree = string.Join("\n", new string[]
+            {
+                $"blob foo hello.txt",
+                $"tree baz sub"
+            });
+
+            dataProviderMock.Setup(f => f.HashObject(It.IsAny<byte[]>(), It.IsAny<string>())).Returns<byte[], string>((data, type)=>
+            {
+                if (data.SequenceEqual(Encoding.UTF8.GetBytes(subStree)) && type =="tree")
+                {
+                    return "baz";
+                }
+
+                if (data.SequenceEqual(Encoding.UTF8.GetBytes(tree)) && type == "tree")
+                {
+                    return "foobar";
+                }
+
+                if (data.SequenceEqual(helloData) && type =="blob")
+                {
+                    return "foo";
+                }
+
+                if(data.SequenceEqual(ugitData) && type == "blob")
+                {
+                    return "bar";
+                }
+                return null;
+            });
+            string expected = "foobar";
+            Assert.AreEqual(expected, baseOperator.WriteTree());
+            fileMock.VerifyAll();
             directoryMock.VerifyAll();
             fileSystemMock.VerifyAll();
+            dataProviderMock.VerifyAll();
         }
     }
 }
