73467234a31943e6745464757cb9050f86171ffc
gaufung <gaufung@outlook.com>
2021-06-28 22:53:16 +0800

    tag operation

---
+++ src/Tindo.UgitCore/Operations/ITagOperation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Tindo.UgitCore.Operations
+{
+    public interface ITagOperation
+    {
+        IEnumerable<string> All { get; }
+
+        void Create(string name, string oid);
+    }
+}
---
+++ src/Tindo.UgitCore/Operations/TagOperation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Logging;
+namespace Tindo.UgitCore.Operations
+{
+    public class TagOperation : ITagOperation
+    {
+        private readonly IDataOperator dataOperator;
+
+        private readonly ILogger<TagOperation> logger;
+
+        public TagOperation(IDataOperator dataOperator, ILoggerFactory loggerFactory)
+        {
+            this.dataOperator = dataOperator;
+            this.logger = loggerFactory.CreateLogger<TagOperation>();
+
+        }
+
+        public IEnumerable<string> All 
+        {
+            get
+            {
+                string prefix = Path.Join(Constants.Refs, Constants.Tags);
+                foreach (var (tagRef, _) in this.dataOperator.GetAllRefs(prefix, false))
+                {
+                    yield return Path.GetRelativePath(prefix, tagRef);
+                }
+            }
+        }
+
+        public void Create(string name, string oid)
+        {
+            string @ref = Path.Join(Constants.Refs, Constants.Tags, name);
+            this.dataOperator.UpdateRef(@ref, RefValue.Create(false, oid));
+        }
+    }
+}
