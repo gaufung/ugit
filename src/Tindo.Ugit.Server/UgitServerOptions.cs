25430f89e415a0a62f3b137ff2734208b004d1a7
Feng Gao <gaufung@outlook.com>
2021-07-24 10:28:12 +0800

    add Tindo.Ugit.Server (#125)

---
+++ src/Tindo.Ugit.Server/UgitServerOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Tindo.Ugit.Server
+{
+    /// <summary>
+    /// Ugit Server Options
+    /// </summary>
+    public class UgitServerOptions
+    {
+        public string RepositoryDirectory { get; set; }
+    }
+}
