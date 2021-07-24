58a56027a4ba6667513e9599db0b24026b19d6ba
gaufung <gaufung@outlook.com>
2021-07-24 10:27:14 +0800

    add Tindo.Ugit.Server

---
+++ src/Tindo.Ugit.Server/Controllers/UgitController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tindo.Ugit.Server.Controllers
+{
+    [Route("api")]
+    public class UgitController : Controller
+    {
+        private readonly ILogger<UgitController> _logger;
+
+        private readonly UgitServerOptions _serverOption;
+
+        public UgitController(IOptions<UgitServerOptions> serverOption, ILogger<UgitController> logger)
+        {
+            _serverOption = serverOption.Value;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok("ugit api" + _serverOption.RepositoryDirectory);
+        }
+    }
+}
---
+++ src/Tindo.Ugit/HttpFileOperator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tindo.Ugit
+{
+    /// <summary>
+    /// Http File Operator.
+    /// </summary>
+    public class HttpFileOperator : IFileOperator
+    {
+        public string CurrentDirectory => throw new NotImplementedException();
+
+        public void CreateDirectory(string directory, bool force = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string path, bool isFile = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EmptyCurrentDirectory(Func<string, bool> ignore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string path, bool isFile = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryRead(string path, out byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Walk(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path, byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
